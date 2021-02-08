29d14e50c4e32b26a3412932b468bfcec52b803a
lib <dreak603@gmail.com>
2021-02-08 12:39:24 +0800

    chore: revise test and adding format scrip

--- src/domains/github/github_create.go
+++ src/domains/github/github_create.go
@@ -1,1 +1,24 @@
 package github
+
+/**
+* @ struct
+{
+   "name": "golang-microservices",
+   "description": "golang-microservices-practice",
+   "homepage": "http://github.com",
+   "private": false,
+   "has_issues": true,
+   "has_projects": true,
+   "has_wiki": true
+}
+*/
+
+type CreateRepoRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+	Private     bool   `json:"private"`
+	HasIssues   bool   `json:"has_issues"`
+	HasProjects bool   `json:"has_projects"`
+	HasWiki     bool   `json:"has_wiki"`
+}
--- src/domains/github/github_create_test.go
+++ src/domains/github/github_create_test.go
@@ -1,1 +1,35 @@
 package github
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateRepoAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "lib",
+		Description: "lib-test",
+		Homepage:    "http://github.com",
+		Private:     false,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+	assert.EqualValues(t, `{"name":"lib","description":"lib-test","homepage":"http://github.com","private":false,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(bytes))
+
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.Description, request.Description)
+	assert.EqualValues(t, target.Homepage, request.Homepage)
+	assert.EqualValues(t, target.Private, request.Private)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+	assert.EqualValues(t, target.HasProjects, request.HasProjects)
+	assert.EqualValues(t, target.HasWiki, request.HasWiki)
+}
